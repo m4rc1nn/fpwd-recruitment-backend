0c87f09af45fa7cc37e456ad7534230832721fbb
root <mkowalczyk193@gmail.com>
2024-10-19 13:31:52 +0200

    test deploy

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 require('dotenv').config()
 
 const express = require('express');
+const exchangeRateRoutes = require('./routes/exchangeRate');
+// const transactionRoutes = require('./routes/transaction');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api', exchangeRateRoutes);
-app.use('/api', transactionRoutes);
+// app.use('/api', transactionRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
--- routes/exchangeRate.js
+++ routes/exchangeRate.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const exchangeRateController = require('../controllers/exchangeRateController');
 
-const cache = require('apicache');
+const apicache = require('apicache');
+
+const cache = apicache.middleware;
 
 router.get('/exchange-rate', cache('1 minute'), exchangeRateController.getExchangeRate);
 
