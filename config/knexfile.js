136c8fb62a175cc3d2e2a5b30bf6c5a1a76aac21
marcin0s <mkowalczyk193@gmail.com>
2024-10-21 22:06:42 +0200

    change filpath and push empty db file

--- config/knexfile.js
+++ config/knexfile.js
@@ -1,17 +1,7 @@
-import fs from "fs";
-import path from "path";
-
-const dbDirectory = path.join(process.cwd(), "tmp");
-
-// Ensure the directory exists
-if (!fs.existsSync(dbDirectory)) {
-    fs.mkdirSync(dbDirectory, { recursive: true });
-}
-
 export default {
-    client: "sqlite3",
-    connection: {
-        filename: path.join(dbDirectory, "transactions.db"),
-    },
-    useNullAsDefault: true,
+  client: 'sqlite3',
+  connection: {
+    filename: 'transactions.db',
+  },
+  useNullAsDefault: true,
 };
