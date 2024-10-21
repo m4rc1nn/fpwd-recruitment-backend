9717bde522b1985dc9fc44ce5c0a94ae72a0ddca
marcin0s <mkowalczyk193@gmail.com>
2024-10-21 22:04:40 +0200

    change filepath

--- config/knexfile.js
+++ config/knexfile.js
@@ -1,10 +1,17 @@
 import fs from "fs";
 import path from "path";
 
+const dbDirectory = path.join(process.cwd(), "tmp");
+
+// Ensure the directory exists
+if (!fs.existsSync(dbDirectory)) {
+    fs.mkdirSync(dbDirectory, { recursive: true });
+}
+
 export default {
     client: "sqlite3",
     connection: {
-        filename: path.join(process.cwd(), "transactions.db"),
+        filename: path.join(dbDirectory, "transactions.db"),
     },
     useNullAsDefault: true,
 };
